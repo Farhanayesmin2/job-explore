901b2bdc853924fad1c4cb10deefe115c5d25807
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-04-10 17:38:06 +0600

    First Git Push from vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      
+      <h1>Hello</h1>
+      <h1>I love ALLAH</h1>
+    </div>
+  )
+}
+
+export default App
