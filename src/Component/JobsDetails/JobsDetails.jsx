c3a7b9547ef5e57507102dc2e1d058bf6b47c4d1
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-04-12 03:19:39 +0600

    job details page data added

--- src/Component/JobsDetails/JobsDetails.jsx
+++ src/Component/JobsDetails/JobsDetails.jsx
@@ -33,29 +33,34 @@ console.log(details.jobTitle);
   <div class="bg-white rounded-lg shadow-lg overflow-hidden mb-4 md:mb-0 md:w-3/4">
     <div class="p-4">
       <h2 class="text-2xl font-bold mb-2">Job Description:</h2>
-      <p class="text-gray-700">A UI/UX (User Interface/User Experience) designer is responsible for designing and creating engaging and effective interfaces for software and web applications. This includes designing the layout, visual design, and interactivity of the user interface.</p>
-      <h2 class="text-2xl font-bold my-2">Job Details:</h2>
-      <p class="text-gray-700">Job Responsibility: Collaborating with cross-functional teams: UI/UX designers often work closely with other teams, including product management, engineering, and marketing, to ensure that the user interface is aligned with business and technical requirements. You will need to be able to effectively communicate your design ideas and gather feedback from other team members.</p>
-      <p class="text-gray-700">Salary: 100K - 150K (Per Month)</p>
-                        <p class="text-gray-700">Job Title: Product Designer</p>
+                        <p class="text-gray-700">{details.description}</p>
+      <h2 class="text-2xl font-bold my-2">Job Responsibility:</h2>
+                        <p class="text-gray-700">{details.jobResponsibility}</p>
+
                         <h2 class="text-xl font-bold mb-2">Educational Requirements:</h2>
                         <p>
-                            Bachelor degree to complete any reputational university.</p>
+                            {details.educationalRequirements}</p>
                         <h2 class="text-xl font-bold mb-2">Experiences:
                             </h2>
-                        <p>2-3 Years in this field.</p>
+                        <p>{details.experience}</p>
     </div>
   </div>
-  <div class="bg-white rounded-lg shadow-lg overflow-hidden md:w-1/4">
-    <div class="p-4">
-      <h2 class="text-2xl font-bold mb-2">Contact Information:</h2>
-      <p class="text-gray-700">Educational Requirements:</p>
-      <p class="text-gray-700">Phone: 01750-00 00 00</p>
-      <p class="text-gray-700">Bachelor degree to complete any reputational university.</p>
-      <p class="text-gray-700">Email: info@gmail.com</p>
-      <p class="text-gray-700">Experiences:</p>
-      <p class="text-gray-700">Address: Dhanmondi 32, Sukrabad Dhaka, Bangladesh</p>
-      <p class="text-gray-700">2-3 Years in this field.</p>
+                <div class="bg-white rounded-lg shadow-lg overflow-hidden md:w-1/4">
+                    
+                    <div class="p-4">
+                        <h2 class="text-2xl font-bold mb-2 my-3">Job Details:</h2>
+                        <hr className='my-4'></hr>
+                        <p>Salary: {details.salary}</p>
+                        <p>Job Title: {details.jobTitle} </p>
+                        <hr className='my-4'></hr>
+      <h2 class="text-xl font-bold mb-2">Contact Information:</h2>
+
+                        <p class="text-gray-700">Phone: {details.phone}</p>
+     
+      <p class="text-gray-700">Email: {details.email}</p>
+      
+                        <p class="text-gray-700">Address: {details.address}</p>
+      
       <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block">Apply Now</a>
     </div>
   </div>
